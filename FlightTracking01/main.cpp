2b30369b3d556797bbb35521e5faca5e5ef65ded
Cobaramin <gamekansompoj@hotmail.com>
2016-04-19 15:11:26 +0700

    Change Some Function & Pointer 4/19/2016

--- FlightTracking01/main.cpp
+++ FlightTracking01/main.cpp
@@ -1,4 +1,5 @@
 #define _CRT_SECURE_NO_DEPRECATE
+#define MAXFLIGHT 5
 #include <stdio.h>
 #include <conio.h>
 #include <string.h>
@@ -30,38 +31,48 @@ struct location {
 };
 
 // Function prototypes
+void getLogo();
 int firstInterface();
 void getTime(tm *timeInfo);
 void getRoutine(location *data);
-void initFlight(flight *f, location *l);
+// void initFlight(flight *f, location *l);
+flight* getFlight(location *l);
 
 int main(void) {
 	/// Initialize Variable & Pointer
 	tm *timeInfo = (tm*)malloc(sizeof(tm));
+	time_t now;
 	int selectChoice;
 	int exitStatus = 0;
+	double seconds;
 
+	time(&now);
+	seconds = difftime(now, mktime(timeInfo));
 	//------------ Routine Part
 
-	//flight **allFlight;
-	flight *f1 = (flight*)malloc(sizeof(flight));
+	flight *allFlight[MAXFLIGHT];
+	//flight *f1 = (flight*)malloc(sizeof(flight));
 	location *l1 = (location*)malloc(sizeof(location));
 
 	getRoutine(l1);
 	
 	printf("\n%s-%s",l1->startnode[0],l1->endnode[l1->numHoop-1]);
 	printf("\n%d",l1->numHoop);
-	initFlight(f1, l1);
-	printf("\nStart location is :%s", f1->locate->startnode[0]);
-	printf("\nDesination is : %s",f1->locate->endnode[f1->locate->numHoop-1]);
-	printf("\nThe number of passenger is %d peopoles",f1->num_people);
+	//initFlight(f1, l1);
+	allFlight[0] = getFlight(l1);
+
+	printf("\nStart location is :%s", allFlight[0]->locate->startnode[0]);
+	printf("\nDesination is : %s", allFlight[0]->locate->endnode[allFlight[0]->locate->numHoop-1]);
+	printf("\nThe number of passenger is %d peopoles", allFlight[0]->num_people);
+
+	_getch();
 
 	//----------------- Interfacing
 
 	while (!exitStatus) {
 		system("cls"); // <--- Clear Screen
 		getTime(timeInfo); // <--- Show current time 
-
+		getLogo();
 
 		printf("\n-----Fight Tracking ver 1.0-----\n");
 
@@ -75,9 +86,6 @@ int main(void) {
 		case '2':
 			exitStatus = 1;
 			break;
-		case '3':
-			exitStatus = 1;
-			break;
 		default:
 			printf("\ntry agian!!\n");
 			break;
@@ -87,6 +95,18 @@ int main(void) {
 
 	_getch();
 	return 0;
+}
+
+void getLogo(){
+	FILE *fp;
+	char buff[80];
+
+	fp = fopen("logo.txt", "r");
+
+	while (fgets(buff,80, fp) != NULL) {
+		printf("%s",buff);
+	}
+	fclose(fp);
 }
 
 void getRoutine(location *data) {
@@ -110,7 +130,29 @@ void getRoutine(location *data) {
 	fclose(fp);
 }
 
-void initFlight(flight *f, location *l) {
+//void initFlight(flight *f, location *l) {
+//	FILE *fp;
+//	char code[50];
+//	double speed;
+//	int numPeople;
+//	int num = 0;
+//
+//	fp = fopen("flight/f01.csv", "r");
+//
+//	while (fscanf(fp, "%s %lf %d", code, &speed, &numPeople) != EOF) {
+//		num++;
+//		printf("\n[%s]-%.2lf->%d", code, speed, numPeople);
+//		
+//		strcpy(f->code, code);
+//		f->locate = l;
+//		f->num_people = numPeople;
+//		f->speed = speed;
+//	}
+//	fclose(fp);
+//}
+
+flight* getFlight(location *l) {
+	flight *f = (flight*)malloc(sizeof(flight));
 	FILE *fp;
 	char code[50];
 	double speed;
@@ -122,30 +164,23 @@ void initFlight(flight *f, location *l) {
 	while (fscanf(fp, "%s %lf %d", code, &speed, &numPeople) != EOF) {
 		num++;
 		printf("\n[%s]-%.2lf->%d", code, speed, numPeople);
-		
+
 		strcpy(f->code, code);
 		f->locate = l;
 		f->num_people = numPeople;
 		f->speed = speed;
 	}
 	fclose(fp);
+	return f;
 }
 
 int firstInterface() {
 	int d;
-	printf("Choose below option:\n");
-	printf("Press -> '1' get passenger status\n");
-	printf("         '2' find flight detail by Start & Desination point\n");
-	printf("         '3' find flight detail by using Flight's Code\n");
+	printf("\tChoose below option:\n");
+	printf("\tPress -> '1' Start Flight Tracking\n");
+	printf("\t         '2' Setting\n");
 	d = _getch();
 	return d;
-}
-
-void passengerStatus() {
-	char name[50];
-	printf("Enter Passenger's Name : ");
-	scanf("%s", name);
-
 }
 
 void getTime(tm *timeInfo) {
@@ -167,7 +202,11 @@ void getTime(tm *timeInfo) {
 	timeInfo->tm_wday = info->tm_wday;
 	timeInfo->tm_yday = info->tm_yday;
 	timeInfo->tm_year = info->tm_year;
+}
 
+void settingMenu() {
+	printf("Select the Flight that you want to setting");
+	
 }
 
 void startCounter(flight f, tm timer) {
